8d4128d4848522b47a2946eca2b9ad39b60d348a
thanner <thannersoares@gmail.com>
2018-01-26 18:49:05 -0200

    Stable Version

--- src/main/java/NewExample.java
+++ src/main/java/NewExample.java
@@ -1,86 +1,63 @@
-import com.complexible.common.openrdf.model.Models2;
 import com.complexible.common.rdf.model.Namespaces;
-import com.complexible.common.rdf.model.Values;
-import com.complexible.stardog.ContextSets;
 import com.complexible.stardog.Stardog;
 import com.complexible.stardog.api.Connection;
 import com.complexible.stardog.api.ConnectionConfiguration;
 import com.complexible.stardog.api.admin.AdminConnection;
 import com.complexible.stardog.api.admin.AdminConnectionConfiguration;
-import com.complexible.stardog.icv.Constraint;
-import com.complexible.stardog.icv.ConstraintFactory;
 import com.complexible.stardog.icv.api.ICVConnection;
 import com.complexible.stardog.reasoning.Proof;
 import com.complexible.stardog.reasoning.ProofWriter;
-import org.openrdf.model.IRI;
-import org.openrdf.model.Model;
-import org.openrdf.model.vocabulary.RDF;
-import org.openrdf.model.vocabulary.RDFS;
 import org.openrdf.rio.RDFFormat;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import static com.complexible.common.openrdf.util.ExpressionFactory.some;
-import static com.complexible.common.openrdf.util.ExpressionFactory.subClassOf;
 
 public class NewExample {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // First need to initialize the Stardog instance which will automatically start the embedded server.
         Stardog aStardog = Stardog.builder().create();
 
         // The db name
         String dbName = "sota";
-        String dataFile = "src/main/resources/sota/sota-data.ttl";
-        String constraintsFile = "src/main/resources/sota/sota-constraints.ttl";
+        Path dataFile = Paths.get("src/main/resources/sota/sota-data.ttl");
+        Path constraintsFile = Paths.get("src/main/resources/sota/sota-constraints.ttl");
+        String username = "admin";
+        String password = "admin";
 
         try {
             // Open an `AdminConnection` to Stardog so we can set up our database for the example
-            try (AdminConnection aAdminConnection = AdminConnectionConfiguration.toEmbeddedServer()
-                    .credentials("admin", "admin")
-                    .connect()) {
+            try (AdminConnection aAdminConnection = AdminConnectionConfiguration.toEmbeddedServer().credentials(username, password).connect()) {
                 // If the example database exists, drop it, so we can create it fresh
-                if (aAdminConnection.list().contains("testICVExample")) {
-                    aAdminConnection.drop("testICVExample");
+                if (aAdminConnection.list().contains(dbName)) {
+                    aAdminConnection.drop(dbName);
                 }
 
                 // create a disk database
-                aAdminConnection.disk("testICVExample").create();
+                aAdminConnection.disk(dbName).create(dataFile);
 
                 // Obtain a connection to the database
-                try (Connection aConns = ConnectionConfiguration
-                        .to("testICVExample")
-                        .reasoning(true)
-                        .credentials("admin", "admin")
-                        .connect()) {
+                try (Connection aConn = ConnectionConfiguration.to(dbName).reasoning(true).credentials(username, password).connect()) {
 
-                    // Load the data in the db while creating it
-                    ConnectionConfiguration aConfig = aAdminConnection.memory(dbName).create(Paths.get(dataFile));
-                    // obtain a connection to the database
-                    Connection aConn = aConfig.connect();
-
+                    /** Method: Constraints **/
                     // Grab an [ICVConnection](http://docs.stardog.com/java/snarl/com/complexible/stardog/icv/api/ICVConnection.html)
                     // so we can add our constraint to the database and start using ICV.
                     ICVConnection aValidator = aConn.as(ICVConnection.class);
-
                     // add the constraints, must do this in a transaction
                     aValidator.begin();
-                    aValidator.addConstraints().format(RDFFormat.TURTLE).file(Paths.get(constraintsFile));
+                    aValidator.addConstraints().format(RDFFormat.TURTLE).file(constraintsFile);
                     aValidator.commit();
 
+                    /** Method: Validate **/
                     // use namespaces to pretty print results
                     Namespaces aNamespaces = aValidator.namespaces();
-
                     // check validity
                     boolean isValid = aValidator.isValid();
-
                     // print validation result
                     System.out.format("The Data is%s valid%n", isValid ? "" : " NOT");
-
                     // if not valid print explanations
                     if (!isValid) {
                         Iterable<Proof> aViolationProofs = aValidator.explain().countLimit(10).mergeExplanations().proofs();
-
                         for (Proof aProof : aViolationProofs) {
                             System.out.println(ProofWriter.toString(aNamespaces, aProof));
                         }
@@ -89,8 +66,8 @@ public class NewExample {
                 }
                 finally {
                     // remove the disk database
-                    if (aAdminConnection.list().contains("testICVExample")) {
-                        aAdminConnection.drop("testICVExample");
+                    if (aAdminConnection.list().contains(dbName)) {
+                        aAdminConnection.drop(dbName);
                     }
                 }
             }
