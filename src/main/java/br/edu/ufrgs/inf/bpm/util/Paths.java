a1ebe02d52b95da7eb496e17b0168909cae4dfda
Thanner Soares <thannersoares@gmail.com>
2018-02-04 20:35:48 -0200

    Maven Update

--- src/main/java/br/edu/ufrgs/inf/bpm/util/Paths.java
+++ src/main/java/br/edu/ufrgs/inf/bpm/util/Paths.java
@@ -2,6 +2,8 @@ package br.edu.ufrgs.inf.bpm.util;
 
 public class Paths {
 
+    public final static String LocalResourcePath = "src/main/resources";
+
     public final static String stardogServerProperties = "src/main/configurations/stardogServer.properties";
     public final static String DataFile = "src/main/resources/sota/sota-data.ttl";
     public final static String ConstraintFile = "src/main/resources/sota/sota-constraints.ttl";
